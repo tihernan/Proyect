b955889f117ec8b7cae9774274962256fd2652a7
Tiffany H <tiffale2013@gmail.com>
2022-10-20 22:26:19 -0600

    Clase de diagramas final

--- src/Clases/Main.java
+++ src/Clases/Main.java
@@ -14,7 +14,9 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        LogIn ventana = new LogIn ();
+        ventana.setVisible(true);
+        
     }
     
 }
